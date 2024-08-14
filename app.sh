ccad9f84834325f02beea945baef780c6945f5f8
doohee323 <doohee323@gmail.com>
2024-08-13 23:16:54 -0700

    debug remotely

--- app.sh
+++ app.sh
@@ -12,50 +12,53 @@ mkdir -p logs
 # Start llamafiles
 echo "Starting llamafile servers..."
 
-"models/embedding_model.llamafile" \
---server \
---nobrowser \
---port "${EMBEDDING_MODEL_PORT}" > logs/embedding_model.log 2>&1 &
-pid=$!
+if [[ "$1" == "e" ]]; then
+  "models/embedding_model.llamafile" \
+  --server \
+  --nobrowser \
+  --port "${EMBEDDING_MODEL_PORT}" > logs/embedding_model.log 2>&1 &
+  pid=$!
 
-#"models/embedding_model.llamafile" \
-#--server \
-#--nobrowser \
-#--port "${EMBEDDING_MODEL_PORT}" \
-#-m models/exaone.gguf -ngl 9999 > logs/embedding_model.log 2>&1 &
-#pid=$!
+  #"models/embedding_model.llamafile" \
+  #--server \
+  #--nobrowser \
+  #--port "${EMBEDDING_MODEL_PORT}" \
+  #-m models/exaone.gguf -ngl 9999 > logs/embedding_model.log 2>&1 &
+  #pid=$!
 
-sleep 20
-err=$?
-if [ "${err}" -ne 0 ]; then
-  echo "Failed to start embedding model llamafile"
-  exit 1
+  sleep 20
+  err=$?
+  if [ "${err}" -ne 0 ]; then
+    echo "Failed to start embedding model llamafile"
+    exit 1
+  fi
+  echo "${pid}" > .pid_embedding_model
+  echo "started embedding model"
 fi
-echo "${pid}" > .pid_embedding_model
-echo "started embedding model"
 
+if [[ "$1" == "g" ]]; then
+  "models/generation_model.llamafile" \
+  --server \
+  --nobrowser \
+  --port "${GENERATION_MODEL_PORT}" > logs/generation_model.log 2>&1 &
+  pid=$!
+  sleep 20
+  err=$?
+  if [ "${err}" -ne 0 ]; then
+    echo "Failed to start generation model llamafile"
+    exit 1
+  fi
+  echo "${pid}" > .pid_generation_model
+  echo "started generation model"
 
-"models/generation_model.llamafile" \
---server \
---nobrowser \
---port "${GENERATION_MODEL_PORT}" > logs/generation_model.log 2>&1 &
-pid=$!
-sleep 20
-err=$?
-if [ "${err}" -ne 0 ]; then
-  echo "Failed to start generation model llamafile"
-  exit 1
+  # Run RAG app
+  python app.py "$@"
+
+  while true; do echo $(date -u) >> out.txt; sleep 5; done
 fi
-echo "${pid}" > .pid_generation_model
-echo "started generation model"
-
-# Run RAG app
-python app.py "$@"
-
-while true; do echo $(date -u) >> out.txt; sleep 5; done
 
 ## Shut down the llamafiles
-#kill "$(cat .pid_embedding_model)"
-#kill "$(cat .pid_generation_model)"
+kill "$(cat .pid_embedding_model)"
+kill "$(cat .pid_generation_model)"
 
-#echo "exited."
+echo "exited."
