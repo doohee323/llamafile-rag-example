838b13f65deb491adbf0942614d51177f7aeba78
doohee323 <doohee323@gmail.com>
2024-08-15 17:53:50 -0700

    web api

--- setup.sh
+++ setup.sh
@@ -25,12 +25,9 @@ if [ ! -f ".env" ]; then cp -v .env.example .env; fi
 # - models/generation_model.llamafile
 #
 # https://github.com/Mozilla-Ocho/llamafile
-#EMBEDDING_MODEL_URL="https://huggingface.co/Mozilla/mxbai-embed-large-v1-llamafile/resolve/main/mxbai-embed-large-v1-f16.llamafile"
-EMBEDDING_MODEL_URL="https://huggingface.co/Mozilla/llava-v1.5-7b-llamafile/resolve/main/llava-v1.5-7b-q4.llamafile"
-#EMBEDDING_MODEL_URL="https://huggingface.co/jartine/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile"
-
+EMBEDDING_MODEL_URL="https://huggingface.co/Mozilla/Meta-Llama-3.1-8B-llamafile/resolve/main/Meta-Llama-3.1-8B.Q6_K.llamafile"
 GENERATION_MODEL_URL="https://huggingface.co/Mozilla/Meta-Llama-3-8B-Instruct-llamafile/resolve/main/Meta-Llama-3-8B-Instruct.Q5_K_M.llamafile"
-#GENERATION_MODEL_URL="https://huggingface.co/Mozilla/Mistral-7B-Instruct-v0.2-llamafile/resolve/main/mistral-7b-instruct-v0.2.Q4_0.llamafile"
+#GENERATION_MODEL_URL="https://huggingface.co/Mozilla/Meta-Llama-3.1-8B-Instruct-llamafile/resolve/main/Meta-Llama-3.1-8B-Instruct.Q6_K.llamafile"
 
 function url_to_filename() {
   url=$1
@@ -57,6 +54,7 @@ then
   filename="$(url_to_filename "${GENERATION_MODEL_URL}")"
   curl -o "${filename}" "${GENERATION_MODEL_URL}" -L
   chmod +x "${filename}"
+  rm -Rf generation_model.llamafile
   ln -s "${filename}" generation_model.llamafile
 fi
 
